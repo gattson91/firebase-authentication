b470172926947bc4c6dd162dfc50bd556e7dad30
Gabrielle Anderson <gabrielleanderson@gabrielles-macbook-pro.local>
2018-05-07 12:44:16 -0400

    initial commit

---
+++ EmailLoginApp/HomeScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeScreenViewController.swift
+//  EmailLoginApp
+//
+//  Created by Gabrielle Anderson on 2018-05-04.
+//  Copyright © 2018 Gabrielle Anderson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+
+class HomeScreenViewController: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
---
+++ Pods/Target Support Files/Pods-EmailLoginApp/Pods-EmailLoginApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmailLoginApp : NSObject
+@end
+@implementation PodsDummy_Pods_EmailLoginApp
+@end
